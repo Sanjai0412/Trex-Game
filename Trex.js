1222dc7cbddb323e6ac2cfa27c4ed0ecc584062c
Sanjai0412 <sanjaikanth44@gmail.com>
2024-12-13 01:01:04 +0530

    After adding score card , game over , update t-rex image after game over,  pop-up after the game is over

--- Trex.js
+++ Trex.js
@@ -4,44 +4,124 @@ const grid=document.getElementById('grid');
 const alert=document.getElementById('alert');
 const desert=document.getElementById('desert');
 
+let scoreDiv=document.getElementById('score');
+let score=0; // to update score
+let gameOver=false;
 let isJumping=false; 
-
-let dinoPosition=150;
-let count=0;
+let dinoPosition=0;
 
 
-function control(e){
-    if(e.code=='Space'){
-       // console.log(e.code);
-       if(!isJumping){
-          jump();
-       }
-    }
+function control(e){ //this function is used to check the clicking buttom is "Space" or not
+
+     if(e.code=='Space'){  // if the code is 'Space' Then call the function 'jump()'
+
+        if(!isJumping){ // if the isJumping=false , then execute the funvtion
+            jump();
+        }
+     }
 }
 
 function jump(){
+    isJumping=true; // setting isJumping = true ,  to avoid multiple jumps on same time
 
-    isJumping=true;
-    let Interval=setInterval(()=>{
-        dinoPosition-=8;
-        count++;
-        dino.style.top=dinoPosition+'px';
-        if(count==8){
-            clearInterval(Interval);
-            dinoPosition=80;
-            let DownInterval=setInterval(()=>{
-                dinoPosition+=8;
-                count--;
-                dino.style.top=dinoPosition+'px';
-                if(count==0){
-                    clearInterval(DownInterval)
-                    isJumping=false;
-                }
-            },20)
+    let upInterval=setInterval(()=>{  // adding 10px on dino 'bottom' position for each 20ms
+          dinoPosition+=10;
+          dino.style.bottom=dinoPosition+'px';
+
+          if(parseInt(dino.style.bottom)==140){ //stop the upInterval when bottom of dino==110, because the dino has reached the maximum height for a jump
+               clearInterval(upInterval);
+                downInterval();
+          }
+    },22)
+}
+
+function downInterval(){
+    let downInterval=setInterval(()=>{   // for reducing the 'bottom px'of the dino to reach the ground
+
+        dinoPosition-=10;
+        dino.style.bottom=dinoPosition+'px';
+
+        if(parseInt(dino.style.bottom)==0){   // if the bottom of dino ==0 , then clear the 'downInterval' .Because dino reached the ground
+            clearInterval(downInterval);
+            isJumping=false;
         }
-    },20)
+   },22)   
+}
+
+function createObstacles(){
+    let alert=document.getElementById('alert');
+
+    if(!gameOver){
+        let obstaclePosition=1300;
+        let obstacle=document.createElement('div');
+        obstacle.className='obstacle';
+       
+              let cactusImage=document.createElement('img');
+              cactusImage.src='cacti.png';
+
+          obstacle.append(cactusImage)
+
+          obstacle.style.position='absolute'
+          obstacle.style.bottom=0+'px'
+          obstacle.style.left=obstaclePosition+'px';    
+          
+        grid.appendChild(obstacle);
+
+        let obstacleInterval=setInterval(()=>{
+
+            score++; // incrementing score
+            scoreDiv.textContent="Your Score : "+score; // displaying the updated score 
+           
+            gameOver=isTouching(dino,obstacle);
+                if(gameOver){
+                    clearInterval(obstacleInterval);
+                    grid.removeChild(grid.lastChild);
+
+                        // T-rex after dead
+                        dinoAfterDead(desert,dino);
+
+                        // Alert Message
+                        alert.innerHTML="Game Over ! ";
+                        let popUp=document.createElement('div');
+                        popUp.id='pop-up';
+                        popUp.textContent='Focus bruh :(';
+                    grid.append(popUp); // append pop-up to the grid
+                    isJumping=true;
+                }
+              obstaclePosition-=10;
+              obstacle.style.left=obstaclePosition+'px';
+
+              if(parseInt( obstacle.style.left)==-80){
+                  clearInterval(obstacleInterval);  
+                  grid.removeChild(grid.lastChild);
+                  createObstacles();
+              }
+          },25)
+    }
+}
+createObstacles()
+
+
+// checking the two objects touching each other or not , returns boolean value
+function isTouching(object1,object2){
+    let touch=false;
+    if(parseInt(object1.style.bottom)<70 && parseInt(object2.style.left)<20){
+      touch=true;
+      gameOver=true;
+    }
+    return touch;
+}
+
+// Style for Trex after his death and stop the bg-animation
+function dinoAfterDead(bg,trex){
+    bg.style.animation='none';
+
+    trex.style.backgroundImage="url('t-rex-dead.png')";
+    trex.style.width=72+'px';
+    trex.style.height=70+'px';
 }
 
 
-document.addEventListener('keydown',control); 
+document.addEventListener('keydown',control);
+
 
